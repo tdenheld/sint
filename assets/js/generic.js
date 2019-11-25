3cf9046fe50a745374f1fa031c8ae7189449892a
Tibor den Held <tibor.denheld@ns.nl>
2019-11-25 14:40:28 +0100

    fix to much removed bugs

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -1,8 +1,31 @@
 'use strict';
 
+const ß = (node, element) => {
+    const obj = element || document;
+    const qs = obj.querySelectorAll(node);
+    return Array.from(qs);
+}
+
+const exists = node => {
+    return document.body.contains(document.querySelector(node));
+}
+
+const isHidden = node => {
+    return window.getComputedStyle(node).display === 'none';
+}
+
+const valueInArray = (value, array) => {
+    return array.includes(value);
+}
+
+const removeAllChilds = node => {
+    while (node.firstChild) node.removeChild(node.firstChild);
+}
+
 const input = () => {
     const input = document.querySelector('[data-type="input"]');
     const overlay = document.querySelector('[data-type="overlay"]');
+    const delay = Modernizr.touchevents ? 0.3 : 0.1;
 
     input.addEventListener('keyup', () => {
         if (input.value === '0906') {
@@ -12,7 +35,7 @@ const input = () => {
 
             gsap.to(overlay, {
                 duration: 0.7,
-                delay: 0.3,
+                delay: delay,
                 ease: 'power4.inOut',
                 opacity: 1,
                 x: 0
