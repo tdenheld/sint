18713e206e1613500f18df0a99ae31a2cebb7924
Tibor den Held <tibor.denheld@ns.nl>
2019-11-26 20:11:03 +0100

    update animations

--- assets/js/generic.js
+++ assets/js/generic.js
@@ -24,22 +24,31 @@ const removeAllChilds = node => {
 
 const input = () => {
     const input = document.querySelector('[data-type="input"]');
-    const overlay = document.querySelector('[data-type="overlay"]');
+    const overlay = '[data-type="overlay"]';
+    const overlayContent = '[data-type="overlay-content"]';
     const delay = Modernizr.touchevents ? 0.3 : 0.1;
 
     input.addEventListener('keyup', () => {
         if (input.value === '0906') {
             input.blur();
-            gsap.fromTo(overlay, {
+            gsap.timeline().fromTo(overlay, {
                 display: 'flex',
                 x: '100vw',
-            },{
+            }, {
                 duration: 0.7,
-                delay: delay,
                 ease: 'power4.inOut',
+                delay: delay,
+                x: 0,
+            }).fromTo(overlayContent, {
+                opacity: 0,
+                x: 100,
+            }, {
+                duration: 0.6,
+                stagger: 0.08,
+                ease: 'power3.out',
                 opacity: 1,
                 x: 0,
-            });
+            }, '<0.4');
         }
     });
 }
